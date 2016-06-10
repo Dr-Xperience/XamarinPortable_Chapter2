a6f42da55f16a5ebe04a1c937a0b7dfdfdf07799
Dr-Xperience <dr.xperience@gmail.com>
2016-06-10 09:06:26 +0530

    Add project files.

---
+++ XamarinPortable_Chapter2/XamarinPortable_Chapter2.Windows/DeviceInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+
+namespace PlatformInfo.Windows
+{
+    public class DeviceInfo : IDeviceInfo
+    {
+        EasClientDeviceInformation vDeviceInfo = new EasClientDeviceInformation();
+        public String Model
+        {
+            get { return String.Format("{0} {1}", vDeviceInfo.SystemManufacturer, vDeviceInfo.SystemProductName); }
+        }
+
+        public String Version
+        {
+            get { return vDeviceInfo.OperatingSystem; }
+        }
+    }
+}
---
+++ XamarinPortable_Chapter2/XamarinPortable_Chapter2/DependencyInjector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency
+{
+    public class DependencyInjector
+    {
+        private PlatformInfo.IDeviceInfo device;
+        public PlatformInfo.IDeviceInfo Device
+        {
+            get { return device; }
+            set { device = value; }
+        }
+        private static DependencyInjector obj = null;
+
+        private DependencyInjector()
+        {
+
+        }
+
+        public static DependencyInjector getDependencyInjector()
+        {
+            if (obj == null)
+            {
+                obj = new DependencyInjector();
+            }
+            return obj;
+        }
+    }
+}
